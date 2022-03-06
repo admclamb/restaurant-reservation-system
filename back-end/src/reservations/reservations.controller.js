7cb1b54dc3aa6d0b8a1615c8068c13839497028f
admclamb <adylanmclamb@yahoo.com>
2022-03-05 21:50:25 -0500

    update all

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -4,9 +4,11 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
  * List handler for reservation resources
  */
 async function list(req, res) {
-  const reservations = await service.list;
+  const reservations = await service.list();
+  console.log(reservations);
+  console.log("here");
   res.json({
-    data: [...reservations],
+    data: reservations,
   });
 }
 
