9dac8f769bcc7f397c6d566b310e2d5ad1c7c3a1
admclamb <adylanmclamb@yahoo.com>
2022-03-06 12:54:18 -0500

    update createReservation

--- front-end/src/newReservation/NewReservation.js
+++ front-end/src/newReservation/NewReservation.js
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const NewReservation = () => {
+  const history = useHistory();
+  const initReservation = {
+    first_name: "",
+    last_name: "",
+    mobile_number: "",
+    \: "",
+    reservation_time: "",
+    people: 0,
+  };
+  const [reservation, setReservation] = useState(initReservation);
+
+  const handleChange = ({ target }) => {
+    const { id } = target;
+    setReservation({
+      ...reservation,
+      [id]: target.value,
+    });
+    return;
+  };
+
+  const handleCancel = () => {
+    setReservation(initReservation);
+    history.push("/");
+    return;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setReservation(initReservation);
+    history.push("/dashboard");
+    return;
+  };
   return (
     <>
       <h1>New Reservation</h1>
@@ -13,6 +46,9 @@ const NewReservation = () => {
               id="first_name"
               className="form-control"
               placeholder="First name"
+              aria-describedby="firstName"
+              value={reservation.firstName}
+              onChange={handleChange}
             />
           </label>
         </div>
@@ -24,6 +60,9 @@ const NewReservation = () => {
               id="last_name"
               className="form-control"
               placeholder="Last name"
+              aria-describedby="lastName"
+              value={reservation.lastName}
+              onChange={handleChange}
             />
           </label>
         </div>
@@ -35,28 +74,39 @@ const NewReservation = () => {
               id="mobile_number"
               className="form-control"
               placeholder="Mobile number"
+              aria-describedby="phoneNumber"
+              value={reservation.mobile_number}
+              onChange={handleChange}
             />
           </label>
         </div>
         <div className="form-group">
-          <label htmlFor="reservation_date">
+          <label htmlFor="date">
             Date of Reservation
             <input
+              type="date"
               name="reservation_date"
-              id="reservation_date"
+              id="date"
               className="form-control"
               placeholder="Reservation date"
+              aria-describedby="date"
+              value={reservation.date}
+              onChange={handleChange}
             />
           </label>
         </div>
         <div className="form-group">
-          <label htmlFor="reservation_time">
+          <label htmlFor="time">
             Time of Reservation
             <input
+              type="time"
               name="reservation_time"
-              id="reservatoin_time"
+              id="time"
               className="form-control"
               placeholder="Reservatoin time"
+              aria-describedby="time"
+              value={reservation.time}
+              onChange={handleChange}
             />
           </label>
         </div>
@@ -68,14 +118,21 @@ const NewReservation = () => {
               id="people"
               className="form-control"
               placeholder="Number of people"
+              aria-describedby="numberOfPeople"
+              value={reservation.numberOfPeople}
+              onChange={handleChange}
             />
           </label>
         </div>
         <div className="">
-          <button type="cancel" className="btn btn-secondary mr-3">
+          <button className="btn btn-secondary mr-3" onClick={handleCancel}>
             Cancel
           </button>
-          <button type="submit" className="btn btn-primary">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleSubmit}
+          >
             Submit
           </button>
         </div>
