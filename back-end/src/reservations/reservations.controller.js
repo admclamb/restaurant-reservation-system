6c848c0a9d145fa1ed251f25f348807b16a575c9
admclamb <adylanmclamb@yahoo.com>
2022-03-10 22:32:11 -0500

    update reservation controller, reservation.router, and reservation.service.

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,17 +1,45 @@
 const service = require("./reservations.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
-/**
- * List handler for reservation resources
- */
+
+const VALID_PROPERTIES = [
+  "first_name",
+  "last_name",
+  "mobile_number",
+  "reservation_date",
+  "reservation_time",
+];
+
+function hasOnlyValidProperties(req, res, next) {
+  const { data = {} } = req.body;
+
+  const invalidFields = Object.keys(data).filter(
+    (field) => !VALID_PROPERTIES.includes(field)
+  );
+
+  if (invalidFields.length) {
+    return next({
+      status: 400,
+      message: `Invalid field(s): ${invalidFields.join(", ")}`,
+    });
+  }
+  next();
+}
+
+
 async function list(req, res) {
   const reservations = await service.list();
-  console.log(reservations);
-  console.log("here");
-  res.json({
+  res.status(201).json({
     data: reservations,
   });
 }
 
+async function create(req, res) {
+  const reservation = req.body.data;
+  const createdReservation = await service.create(reservation);
+  res.status(201).json({ data: createdReservation });
+}
+
 module.exports = {
   list: asyncErrorBoundary(list),
+  create,
 };
