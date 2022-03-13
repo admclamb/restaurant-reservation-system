43172eaf9a9acf8d67010775d2aeeb0b34a240fe
admclamb <adylanmclamb@yahoo.com>
2022-03-12 20:55:17 -0500

    update new reservation

--- front-end/src/newReservation/NewReservation.js
+++ front-end/src/newReservation/NewReservation.js
@@ -3,7 +3,6 @@ import { useHistory } from "react-router-dom";
 import { createReservation } from "../utils/api";
 
 const NewReservation = () => {
-  const history = useHistory();
   const initReservation = {
     first_name: "",
     last_name: "",
@@ -13,7 +12,8 @@ const NewReservation = () => {
     people: 0,
   };
   const [reservation, setReservation] = useState(initReservation);
-
+  const [validated, setValidated] = useState(false);
+  const history = useHistory();
   const handleChange = ({ target }) => {
     const { id } = target;
     setReservation({
@@ -30,51 +30,65 @@ const NewReservation = () => {
   };
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
     const createdReservation = await createReservation(reservation);
     setReservation(initReservation);
+    setValidated(true);
     history.push("/dashboard");
     return;
   };
 
-  console.log(reservation);
   return (
-    <>
+    <main className="container pt-3 mb-5">
       <h1>New Reservation</h1>
-      <form>
-        <div className="form-group">
-          <label htmlFor="first_name">
-            First Name
-            <input
-              name="first_name"
-              id="first_name"
-              className="form-control"
-              placeholder="First name"
-              aria-describedby="firstName"
-              value={reservation.first_name}
-              onChange={handleChange}
-            />
-          </label>
+      <form
+        className="row g-3 needs-validation"
+        id="new-reservation-form"
+        noValidate
+        validated={validated}
+        onSubmit={handleSubmit}
+      >
+        <div className="col-md-4">
+          <label htmlFor="first_name">First Name</label>
+          <input
+            name="first_name"
+            id="first_name"
+            className="form-control"
+            placeholder="First name"
+            aria-describedby="firstName"
+            value={reservation.first_name}
+            onChange={handleChange}
+          />
+          <div className="invalid-feedback">Must provide a fist name.</div>
+          <div className="valid-feedback">Looks Good!</div>
         </div>
-        <div className="form-group">
-          <label htmlFor="last_name">
-            Last Name
-            <input
-              name="last_name"
-              id="last_name"
-              className="form-control"
-              placeholder="Last name"
-              aria-describedby="lastName"
-              value={reservation.last_name}
-              onChange={handleChange}
-            />
-          </label>
+
+        <div className="col-md-4">
+          <label htmlFor="last_name">Last Name</label>
+          <input
+            name="last_name"
+            id="last_name"
+            className="form-control"
+            placeholder="Last name"
+            aria-describedby="lastName"
+            value={reservation.last_name}
+            onChange={handleChange}
+          />
+          <div className="invalid-feedback">Must provide a last name.</div>
+          <div className="valid-feedback">Looks Good!</div>
         </div>
-        <div className="form-group">
+        <div className="col-md-4">
           <label htmlFor="mobile_number">
             Mobile Number
             <input
               name="mobile_number"
+              type="tel"
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
               id="mobile_number"
               className="form-control"
               placeholder="Mobile number"
@@ -82,66 +96,70 @@ const NewReservation = () => {
               value={reservation.mobile_number}
               onChange={handleChange}
             />
+            <div className="invalid-feedback">Must provide a phone number.</div>
+            <div className="valid-feedback">Looks Good!</div>
           </label>
         </div>
-        <div className="form-group">
-          <label htmlFor="date">
-            Date of Reservation
-            <input
-              type="date"
-              name="reservation_date"
-              id="reservation_date"
-              className="form-control"
-              placeholder="Reservation date"
-              aria-describedby="date"
-              value={reservation.reservation_date}
-              onChange={handleChange}
-            />
-          </label>
+        <div className="col-md-3">
+          <label htmlFor="date">Date of Reservation</label>
+          <input
+            type="date"
+            name="reservation_date"
+            id="reservation_date"
+            className="form-control"
+            placeholder="Reservation date"
+            aria-describedby="date"
+            value={reservation.reservation_date}
+            onChange={handleChange}
+          />
+          <div className="invalid-feedback">Must provide a date.</div>
+          <div className="valid-feedback">Looks Good!</div>
         </div>
-        <div className="form-group">
-          <label htmlFor="time">
-            Time of Reservation
-            <input
-              type="time"
-              name="reservation_time"
-              id="reservation_time"
-              className="form-control"
-              placeholder="Reservatoin time"
-              aria-describedby="time"
-              value={reservation.reservation_time}
-              onChange={handleChange}
-            />
-          </label>
+        <div className="col-md-3">
+          <label htmlFor="time">Time of Reservation</label>
+          <input
+            type="time"
+            name="reservation_time"
+            id="reservation_time"
+            className="form-control"
+            placeholder="Reservation time"
+            aria-describedby="time"
+            value={reservation.reservation_time}
+            onChange={handleChange}
+          />
+          <div className="invalid-feedback">Must provide a time.</div>
+          <div className="valid-feedback">Looks Good!</div>
         </div>
-        <div className="form-group">
-          <label htmlFor="people">
-            Number of People
-            <input
-              name="people"
-              id="people"
-              className="form-control"
-              placeholder="Number of people"
-              aria-describedby="numberOfPeople"
-              value={reservation.people}
-              onChange={handleChange}
-            />
-          </label>
+        <div className="col-3">
+          <label htmlFor="people">Number of People</label>
+          <input
+            type="number"
+            name="people"
+            id="people"
+            className="form-control"
+            placeholder="Number of people"
+            aria-describedby="numberOfPeople"
+            value={reservation.people}
+            onChange={handleChange}
+          />
+          <div className="invalid-feedback">
+            Must provide the amount of people and cannot be zero.
+          </div>
+          <div className="valid-feedback">Looks Good!</div>
         </div>
-        <div className="">
-          <button className="btn btn-secondary mr-3" onClick={handleCancel}>
-            Cancel
-          </button>
+        <div className="form-btns">
+          <button className="btn btn-secondary me-3">Cancel</button>
           <button
-            type="submit"
             className="btn btn-primary"
-            onClick={handleSubmit}
+            type="submit"
+            form="new-reservation-form"
+            value="Submit"
           >
             Submit
           </button>
         </div>
       </form>
-    </>
+    </main>
   );
 };
 
