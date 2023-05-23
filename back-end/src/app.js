2fdb639cf92e702f029be352bebf0d86b23cfe34
Anthony Mclamb <adylanmclamb@yahoo.com>
2023-05-23 11:59:13 -0400

    Increate securit y

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,8 +1,12 @@
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
-
+const { FRONT_END_URL } = process.env;
 const express = require("express");
 const cors = require("cors");
+
+const corsOptions = {
+  origin: FRONT_END_URL,
+};
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
@@ -10,7 +14,7 @@ const reservationsRouter = require("./reservations/reservations.router");
 const tablesRouter = require("./tables/tables.router");
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/reservations", reservationsRouter);
